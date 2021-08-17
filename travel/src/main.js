f788b9bb6760953a2502f87ce7349a192b79c392
Ting-Chan <1401971041@qq.com>
2021-08-17 17:17:33 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
+import './assets/style/normalize.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+
+// 解决移动端300ms点击事件延迟
+fastClick.attach(document.body)
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
