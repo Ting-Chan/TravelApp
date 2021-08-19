f4efd249362cda7ebd24f80c495ca35552f02ec5
Ting-Chan <1401971041@qq.com>
2021-08-20 01:22:02 +0800

    add home-swiper

---
+++ travel-app/src/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true,
+      },
+      swiperList: [
+        {
+          id: "001",
+          imgUrl:
+            "https://b3-q.mafengwo.net/s13/M00/71/D6/wKgEaVyd7i6AFjCjAADni5x-vvY89.jpeg",
+        },
+        {
+          id: "002",
+          imgUrl:
+            "https://n3-q.mafengwo.net/s13/M00/FD/07/wKgEaVycp3-AVimPAADWiqvk6ek72.jpeg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff !important
+.wrapper
+  width 100%
+  height 0
+  padding-bottom 53.87%
+  overflow hidden
+  background-color #ccc
+  .swiper-img
+    width 100%
+</style>
--- travel-app/src/main.js
+++ travel-app/src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
 import './assets/style/normalize.css'
 import './assets/style/border.css'
 import './assets/style/iconfont.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 // 解决移动端300ms点击事件延迟
 fastClick.attach(document.body)
