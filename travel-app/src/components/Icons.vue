b65d56099c7482a037e632232d90065ccf46a3ad
Ting-Chan <1401971041@qq.com>
2021-08-20 22:23:13 +0800

    add icons

---
+++ travel-app/src/components/Icons.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIcons",
+  data() {
+    return {
+      swiperOption: {},
+      iconList: [
+        {
+          id: "0001",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/hotel.png",
+          desc: "景点门票",
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/flight.png",
+          desc: "滑雪季",
+        },
+        {
+          id: "0003",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/train.png",
+          desc: "泡温泉",
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "东湖苑",
+        },
+        {
+          id: "0005",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/hotel.png",
+          desc: "景点门票",
+        },
+        {
+          id: "0006",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/flight.png",
+          desc: "滑雪季",
+        },
+        {
+          id: "0007",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/train.png",
+          desc: "泡温泉",
+        },
+        {
+          id: "0008",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "东湖苑",
+        },
+        {
+          id: "0009",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "一日游",
+        },
+      ],
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/style/varibels.styl'
+@import '~@/assets/style/mixins.styl'
+.icons >>> .swiper-container
+  overflow hidden
+  height 0
+  padding-bottom 50%
+.icon
+  position relative
+  float left
+  width 25%
+  height 0px
+  padding-bottom 25%
+  .icon-img
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom 0.44rem
+    box-sizing border-box
+    padding 0.1rem
+    .icon-img-content
+      display block
+      margin 0 auto
+      height 100%
+  .icon-desc
+    position absolute
+    margin 0
+    left 0
+    right 0
+    bottom 0
+    height 0.44rem
+    line-height 0.44rem
+    color $darkTextColor
+    text-align center
+    ellipsis()
+</style>
